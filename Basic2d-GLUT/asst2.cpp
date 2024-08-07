6d1ede5d4681a65b8173cb20dac27343d7b05603
kevindskim <kevindskim@naver.com>
2024-08-07 11:33:25 +0900

    초기 커밋.

---
+++ Basic2d-GLUT/asst2.cpp
@@ -0,0 +1,594 @@
+/*******************************************************************************
+ *
+ *   Harvard Computer Science
+ *   CS 175: Computer Graphics
+ *   Professor Steven Gortler
+ *   Students: Michael {Tingley, Traver}
+ *   Emails: {michaeltingley, mtraver}@college.harvard.edu
+ *
+ ******************************************************************************/
+ // Modified by DS to clear texture buffer activation before drawing the second object on August 5, 2024
+
+#include <vector>
+#include <string>
+#include <memory>
+#include <stdexcept>
+#include <stdio.h>
+#if __GNUG__
+#   include <tr1/memory>
+#endif
+
+#include <GL/glew.h>
+#ifdef __MAC__
+#   define IS_MAC true
+#   include <GLUT/glut.h>
+#else
+#   define IS_MAC false
+#   include <GL/glut.h>
+#endif
+
+#include "ppm.h"
+#include "glsupport.h"
+
+ // added by ds to fix compile error C4996
+#pragma warning(disable : 4996)
+
+using namespace std;      // for string, vector, iostream and other standard C++ stuff
+using namespace std::tr1; // for shared_ptr
+
+/* G L O B A L S **************************************************/
+
+/* !!!!!!!! IMPORTANT !!!!!!!!!!!!!!!!!!!!!!!!!!
+ * Before you start working on this assignment, set the following variable properly
+ * to indicate whether you want to use OpenGL 2.x with GLSL 1.0 or OpenGL 3.x+ with
+ * GLSL 1.3.
+ *
+ * Set g_Gl2Compatible = true to use GLSL 1.0 and g_Gl2Compatible = false to use GLSL 1.3.
+ * Make sure that your machine supports the version of GLSL you are using. In particular,
+ * on Mac OS X currently there is no way of using OpenGL 3.x with GLSL 1.3 when
+ * GLUT is used.
+ *
+ * If g_Gl2Compatible=true, shaders with -gl2 suffix will be loaded.
+ * If g_Gl2Compatible=false, shaders with -gl3 suffix will be loaded.
+ * To complete the assignment you only need to edit the shader files that get loaded
+ * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
+static const bool g_Gl2Compatible = false;
+static const float g_initialWidth  = 512.0;
+static const float g_initialHeight = g_initialWidth;
+
+static int g_width             = g_initialWidth;  /** screen width */
+static int g_height            = g_initialHeight; /** screen height */
+static bool g_leftClicked      = false;     /** is the left mouse button down? */
+static bool g_rightClicked     = false;     /** is the right mouse button down? */
+static float g_objScale        = 1.0;       /** scale factor for object */
+static int g_leftClickX, g_leftClickY;      /** coordinates for mouse left click event */
+static int g_rightClickX, g_rightClickY;    /** coordinates for mouse right click event */
+
+/**
+ * Represents the offset of the triangle in the x-direction. This increments
+ * each time the shape should move to the right, and decrements each time the
+ * shape should move to the left.
+ */
+static int g_xOffset           = 0.0;
+
+/** Like xOffset, but in the y direction. */
+static int g_yOffset           = 0.0;
+
+/** Global shader states */
+struct SquareShaderState {
+  GlProgram program;
+
+  /** Handles to uniform variables */
+  GLint h_uVertexScale;
+  GLint h_uTex0, h_uTex1;
+  GLint h_uXCoefficient, h_uYCoefficient;
+
+  /** Handles to vertex attributes */
+  GLint h_aPosition;
+  GLint h_aTexCoord;
+};
+
+struct TriangleShaderState {
+  GlProgram program;
+
+  /** Handles to uniform variables */
+  GLint h_uTex2;
+  GLint h_uXCoefficient, h_uYCoefficient;
+  GLint h_uXOffset, h_uYOffset;
+
+  /** Handles to vertex attributes */
+  GLint h_aPosition;
+  GLint h_aTexCoord;
+  GLint h_aColor;
+};
+
+static shared_ptr<SquareShaderState> g_squareShaderState;
+static shared_ptr<TriangleShaderState> g_triangleShaderState;
+
+/** Global texture instance */
+static shared_ptr<GlTexture> g_tex0, g_tex1, g_tex2;
+
+/** Global geometries to draw a triangle with indecies */ 
+struct GeometryPX {
+  GlBufferObject posVbo, texVbo, colorVbo, indexVbo;
+};
+
+static shared_ptr<GeometryPX> g_square;
+static shared_ptr<GeometryPX> g_triangle;
+
+
+/* C A L L B A C K S **************************************************/
+
+static void drawSquare() {
+  /* Activate the glsl program */
+  glUseProgram(g_squareShaderState->program);
+
+  /* Bind textures */
+  // Activate the texture unit first before binding texture
+  GLuint texHandle0 = g_tex0->getHandle();
+
+  glActiveTexture(GL_TEXTURE0 + texHandle0);
+  glBindTexture(GL_TEXTURE_2D, texHandle0);
+
+  // Activate the texture unit second before binding texture
+  GLuint texHandle1 = g_tex1->getHandle();
+
+  glActiveTexture(GL_TEXTURE0 + texHandle1);
+  glBindTexture(GL_TEXTURE_2D, texHandle1);
+
+  /* Compute coefficients for maintaining aspect ratio */
+  float scaleCoefficient = min(g_width / g_initialWidth, g_height / g_initialHeight);
+
+  /* Set glsl uniform variables */
+  safe_glUniform1i(g_squareShaderState->h_uTex0, texHandle0); /* texHandle0 is 0 as the new values to be used for the specified uniform variable.*/
+  safe_glUniform1i(g_squareShaderState->h_uTex1, texHandle1); /* texHandle1 is 1 as the new values to be used for the specified uniform variable.*/
+  safe_glUniform1f(g_squareShaderState->h_uVertexScale, g_objScale);
+  safe_glUniform1f(g_squareShaderState->h_uXCoefficient, g_initialWidth / g_width * scaleCoefficient);
+  safe_glUniform1f(g_squareShaderState->h_uYCoefficient, g_initialHeight / g_height * scaleCoefficient);
+
+  /* Bind vertex buffers */
+  glBindBuffer(GL_ARRAY_BUFFER, g_square->posVbo);
+  safe_glVertexAttribPointer(g_squareShaderState->h_aPosition,
+                             2, GL_FLOAT, GL_FALSE, 0, 0);
+
+  glBindBuffer(GL_ARRAY_BUFFER, g_square->texVbo);
+  safe_glVertexAttribPointer(g_squareShaderState->h_aTexCoord,
+                             2, GL_FLOAT, GL_FALSE, 0, 0);
+
+  safe_glEnableVertexAttribArray(g_squareShaderState->h_aPosition);
+  safe_glEnableVertexAttribArray(g_squareShaderState->h_aTexCoord);
+
+  // Bind the index buffer and draw elements
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, g_square->indexVbo);
+  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+  safe_glDisableVertexAttribArray(g_squareShaderState->h_aPosition);
+  safe_glDisableVertexAttribArray(g_squareShaderState->h_aTexCoord);
+
+  /* Check for errors */
+  checkGlErrors();
+}
+
+static void drawTriangle() {
+
+  /* Activate the glsl program */
+  glUseProgram(g_triangleShaderState->program);
+
+  /* Bind textures */
+  // Activate the texture unit first before binding texture
+  GLuint texHandle2 = g_tex2->getHandle();
+  glActiveTexture(GL_TEXTURE0 + texHandle2);
+  glBindTexture(GL_TEXTURE_2D, texHandle2);
+
+  /* Compute coefficients for maintaining aspect ratio */
+  float scaleCoefficient = min(g_width / g_initialWidth, g_height / g_initialHeight);
+
+  /* Set glsl uniform variables */
+  safe_glUniform1i(g_triangleShaderState->h_uTex2, texHandle2); /* texHandle2 is 2 as the new values to be used for the specified uniform variable. */
+  safe_glUniform1f(g_triangleShaderState->h_uXCoefficient, g_initialWidth / g_width * scaleCoefficient);
+  safe_glUniform1f(g_triangleShaderState->h_uYCoefficient, g_initialHeight / g_height * scaleCoefficient);
+
+  /* Uniform variables used to move the triangle around the screen */
+  safe_glUniform1f(g_triangleShaderState->h_uXOffset, g_xOffset * .05);
+  safe_glUniform1f(g_triangleShaderState->h_uYOffset, g_yOffset * .05);
+
+  /* Bind vertex buffers */
+  glBindBuffer(GL_ARRAY_BUFFER, g_triangle->posVbo);
+
+  safe_glVertexAttribPointer(g_triangleShaderState->h_aPosition,
+                             2, GL_FLOAT, GL_FALSE, 0, 0);
+
+  glBindBuffer(GL_ARRAY_BUFFER, g_triangle->texVbo);
+  safe_glVertexAttribPointer(g_triangleShaderState->h_aTexCoord,
+                             2, GL_FLOAT, GL_FALSE, 0, 0);
+
+  glBindBuffer(GL_ARRAY_BUFFER, g_triangle->colorVbo);
+  safe_glVertexAttribPointer(g_triangleShaderState->h_aColor,
+                             3, GL_FLOAT, GL_FALSE, 0, 0);
+
+  safe_glEnableVertexAttribArray(g_triangleShaderState->h_aPosition);
+  safe_glEnableVertexAttribArray(g_triangleShaderState->h_aTexCoord);
+  safe_glEnableVertexAttribArray(g_triangleShaderState->h_aColor);
+
+  // Bind the index buffer and draw elements
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, g_triangle->indexVbo);
+  glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+
+
+  safe_glDisableVertexAttribArray(g_triangleShaderState->h_aPosition);
+  safe_glDisableVertexAttribArray(g_triangleShaderState->h_aTexCoord);
+  safe_glDisableVertexAttribArray(g_triangleShaderState->h_aColor);
+
+  /* Check for errors */
+  checkGlErrors();
+}
+
+/**
+ * Display
+ *
+ * Whenever OpenGL requires a screen refresh it will call display() to draw the
+ * scene. We specify that this is the correct function to call with the
+ * glutDisplayFunc() function during initialization.
+ */
+static void display(void) {
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  drawSquare();
+  drawTriangle();
+
+  glutSwapBuffers();
+
+  /* check for errors */
+  checkGlErrors();
+}
+
+
+/**
+ * Reshape
+ *
+ * Whenever a window is resized, a "resize" event is generated and glut is told
+ * to call this reshape callback function to handle it appropriately.
+ */
+static void reshape(int w, int h) {
+  g_width = w;
+  g_height = h;
+  glViewport(0, 0, w, h);
+  glutPostRedisplay();
+}
+
+
+/**
+ * Mouse
+ *
+ * Whenever a mouse button is clicked, a "mouse" event is generated and this
+ * mouse callback function is called to handle the user input.
+ */
+
+static void mouse(int button, int state, int x, int y) {
+  if (button == GLUT_LEFT_BUTTON) {
+    if (state == GLUT_DOWN) {
+      /* right mouse button has been clicked */
+      g_leftClicked = true;
+      g_leftClickX = x;
+      g_leftClickY = g_height - y - 1;
+    }
+    else {
+      /* right mouse button has been released */
+      g_leftClicked = false;
+    }
+  }
+  if (button == GLUT_RIGHT_BUTTON) {
+    if (state == GLUT_DOWN) {
+      /* right mouse button has been clicked */
+      g_rightClicked = true;
+      g_rightClickX = x;
+      g_rightClickY = g_height - y - 1;
+    }
+    else {
+      /* right mouse button has been released */
+      g_rightClicked = false;
+    }
+  }
+}
+
+/**
+ * Motion
+ *
+ * Whenever the mouse is moved while a button is pressed, a "mouse move" event
+ * is triggered and this callback is called to handle the event.
+ */
+static void motion(int x, int y) {
+  const int newx = x;
+  const int newy = g_height - y - 1;
+  if (g_leftClicked) {
+    g_leftClickX = newx;
+    g_leftClickY = newy;
+  }
+  if (g_rightClicked) {
+    float deltax = (newx - g_rightClickX) * 0.02;
+    g_objScale += deltax;
+
+    g_rightClickX = newx;
+    g_rightClickY = newy;
+  }
+  glutPostRedisplay();
+}
+
+static void keyboard(unsigned char key, int x, int y) {
+  switch (key) {
+  case 'h':
+    cout << " ============== H E L P ==============\n\n"
+    << "h\t\thelp menu\n"
+    << "s\t\tsave screenshot\n"
+    << "drag right mouse to change square size\n";
+    break;
+  case 'q':
+    exit(0);
+  case 'i':
+    g_yOffset++;
+    break;
+  case 'j':
+    g_xOffset--;
+    break;
+  case 'k':
+    g_yOffset--;
+    break;
+  case 'l':
+    g_xOffset++;
+    break;
+  case 's':
+    glFinish();
+    writePpmScreenshot(g_width, g_height, "out.ppm");
+    break;
+  }
+  glutPostRedisplay();
+}
+
+/* H E L P E R    F U N C T I O N S ***********************************/
+
+static void initGlutState(int argc, char **argv) {
+  glutInit(&argc,argv);
+  glutInitDisplayMode(GLUT_RGBA|GLUT_DOUBLE|GLUT_DEPTH);
+  glutInitWindowSize(g_width, g_height);      /* create a window */
+  glutCreateWindow("CS 175: Hello World");    /* title the window */
+
+  glutDisplayFunc(display);                   /* display rendering callback */
+  glutReshapeFunc(reshape);                   /* window reshape callback */
+  glutMotionFunc(motion);                     /* mouse movement callback */
+  glutMouseFunc(mouse);                       /* mouse click callback */
+  glutKeyboardFunc(keyboard);
+}
+
+static void initGLState() {
+  glClearColor(128./255,200./255,1,0);
+  glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+  glPixelStorei(GL_PACK_ALIGNMENT, 1);
+  if (!g_Gl2Compatible)
+    glEnable(GL_FRAMEBUFFER_SRGB);
+}
+
+static void loadSquareShader(SquareShaderState& ss) {
+  const GLuint h = ss.program; /* Short hand */
+
+  readAndCompileShader(ss.program, "shaders/asst2-sq-gl3.vshader", "shaders/asst2-sq-gl3.fshader");
+ 
+
+  /* Retrieve handles to uniform variables */
+  ss.h_uVertexScale = safe_glGetUniformLocation(h, "uVertexScale");
+  ss.h_uTex0 = safe_glGetUniformLocation(h, "uTex0");
+  ss.h_uTex1 = safe_glGetUniformLocation(h, "uTex1");
+  ss.h_uXCoefficient = safe_glGetUniformLocation(h, "uXCoefficient");
+  ss.h_uYCoefficient = safe_glGetUniformLocation(h, "uYCoefficient");
+
+  /* Retrieve handles to vertex attributes */
+  ss.h_aPosition = safe_glGetAttribLocation(h, "aPosition");
+  ss.h_aTexCoord = safe_glGetAttribLocation(h, "aTexCoord");
+
+  if (!g_Gl2Compatible)
+    glBindFragDataLocation(h, 0, "fragColor");
+  checkGlErrors();
+}
+
+static void loadTriangleShader(TriangleShaderState& ss) {
+  const GLuint h = ss.program; /* Short hand */
+
+  readAndCompileShader(ss.program, "shaders/asst2-tr-gl3.vshader", "shaders/asst2-tr-gl3.fshader");
+
+
+  /* Retrieve handles to uniform variables */
+  ss.h_uTex2 = safe_glGetUniformLocation(h, "uTex2");
+  ss.h_uXCoefficient = safe_glGetUniformLocation(h, "uXCoefficient");
+  ss.h_uYCoefficient = safe_glGetUniformLocation(h, "uYCoefficient");
+  ss.h_uXOffset = safe_glGetUniformLocation(h, "uXOffset");
+  ss.h_uYOffset = safe_glGetUniformLocation(h, "uYOffset");
+
+  /* Retrieve handles to vertex attributes */
+  ss.h_aPosition = safe_glGetAttribLocation(h, "aPosition");
+  ss.h_aTexCoord = safe_glGetAttribLocation(h, "aTexCoord");
+  ss.h_aColor = safe_glGetAttribLocation(h, "aColor");
+
+  if (!g_Gl2Compatible)
+    glBindFragDataLocation(h, 0, "fragColor");
+  checkGlErrors();
+}
+
+static void initShaders() {
+  g_squareShaderState.reset(new SquareShaderState);
+  loadSquareShader(*g_squareShaderState);
+
+  g_triangleShaderState.reset(new TriangleShaderState);
+  loadTriangleShader(*g_triangleShaderState);
+}
+
+static void loadSquareGeometry(const GeometryPX& g) {
+	const int vdim = 4; /* 4 vertices */
+	const int idim = 6; /* 6 indices */
+  GLfloat pos[2 * vdim] = {
+    -.5, -.5,
+    .5,  .5,
+    .5,  -.5,
+    -.5, .5,
+  };
+
+  GLfloat tex[2 * vdim] = {
+    0, 0,
+    1, 1,
+    1, 0,
+    0, 1,
+  };
+
+  GLuint indices[idim] = { 0, 2, 1, 0, 1, 3 };
+
+  glBindBuffer(GL_ARRAY_BUFFER, g.posVbo);
+  glBufferData(
+    GL_ARRAY_BUFFER,
+    2*vdim*sizeof(GLfloat),
+    pos,
+    GL_STATIC_DRAW);
+  checkGlErrors();
+
+  glBindBuffer(GL_ARRAY_BUFFER, g.texVbo);
+  glBufferData(
+    GL_ARRAY_BUFFER,
+    2*vdim*sizeof(GLfloat),
+    tex,
+    GL_STATIC_DRAW);
+  checkGlErrors();
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, g.indexVbo);
+  glBufferData(
+	  GL_ELEMENT_ARRAY_BUFFER,
+	  idim * sizeof(GLuint),
+	  indices,
+	  GL_STATIC_DRAW);
+  checkGlErrors();
+}
+
+static void loadTriangleGeometry(const GeometryPX& g) {
+	const int vdim = 3; /* 3 vertices */
+	const int idim = 3; /* 3 indices */
+
+  GLfloat pos[2 * vdim] = {
+    0.0, -0.45,
+    -0.45, 0.45,
+    0.45, 0.45
+  };
+
+  /* Center shield in triangle */
+  GLfloat tex[2 * vdim] = {
+    0.5, -.60,
+    -.35, 1.1,
+    1.35, 1.1
+  };
+
+  /* Give each vertex a different color */
+  GLfloat color[3 * vdim] = {
+    1, 0, 0,
+    0, 1, 0,
+    0, 0, 1
+  };
+
+  GLuint indices[idim] = { 0, 2, 1};
+
+  glBindBuffer(GL_ARRAY_BUFFER, g.posVbo);
+  glBufferData(
+    GL_ARRAY_BUFFER,
+    2*vdim*sizeof(GLfloat),
+    pos,
+    GL_STATIC_DRAW);
+  checkGlErrors();
+
+  glBindBuffer(GL_ARRAY_BUFFER, g.texVbo);
+  glBufferData(
+    GL_ARRAY_BUFFER,
+    2*vdim*sizeof(GLfloat),
+    tex,
+    GL_STATIC_DRAW);
+  checkGlErrors();
+
+  glBindBuffer(GL_ARRAY_BUFFER, g.colorVbo);
+  glBufferData(
+    GL_ARRAY_BUFFER,
+    3*vdim*sizeof(GLfloat),
+    color,
+    GL_STATIC_DRAW);
+  checkGlErrors();
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, g.indexVbo);
+  glBufferData(
+      GL_ELEMENT_ARRAY_BUFFER,
+      idim * sizeof(GLuint),
+      indices,
+      GL_STATIC_DRAW);
+  checkGlErrors();
+
+}
+
+static void initGeometry() {
+  g_square.reset(new GeometryPX());
+  loadSquareGeometry(*g_square);
+
+  g_triangle.reset(new GeometryPX());
+  loadTriangleGeometry(*g_triangle);
+}
+
+static void loadTexture(GLuint texHandle, const char *ppmFilename) {
+  int texWidth, texHeight;
+  vector<PackedPixel> pixData;
+
+  ppmRead(ppmFilename, texWidth, texHeight, pixData);
+
+  glActiveTexture(GL_TEXTURE0 + texHandle);
+  glBindTexture(GL_TEXTURE_2D, texHandle);
+  glTexImage2D(GL_TEXTURE_2D, 0, g_Gl2Compatible ? GL_RGB : GL_SRGB, texWidth, texHeight,
+               0, GL_RGB, GL_UNSIGNED_BYTE, &pixData[0]);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+
+  checkGlErrors();
+}
+
+static void initTextures() {
+  g_tex0.reset(new GlTexture());
+  g_tex1.reset(new GlTexture());
+  g_tex2.reset(new GlTexture());
+
+  loadTexture(g_tex0->getHandle(), "smiley.ppm");
+  loadTexture(g_tex1->getHandle(), "reachup.ppm");
+  loadTexture(g_tex2->getHandle(), "shield.ppm");
+}
+
+/* M A I N ************************************************************/
+
+/**
+ * Main
+ *
+ * The main entry-point for the HelloWorld example application.
+ */
+int main(int argc, char **argv) {
+  try {
+    initGlutState(argc,argv);
+
+    glewInit(); // load the OpenGL extensions
+
+    cout << (g_Gl2Compatible ? "Will use OpenGL 2.x / GLSL 1.0" : "Will use OpenGL 3.x / GLSL 1.3") << endl;
+    if ((!g_Gl2Compatible) && !GLEW_VERSION_3_0)
+      throw runtime_error("Error: card/driver does not support OpenGL Shading Language v1.3");
+    else if (g_Gl2Compatible && !GLEW_VERSION_2_0)
+      throw runtime_error("Error: card/driver does not support OpenGL Shading Language v1.0");
+
+    initGLState();
+    initShaders();
+    initGeometry();
+    initTextures();
+
+    glutMainLoop();
+    return 0;
+  }
+  catch (const runtime_error& e) {
+    cout << "Exception caught: " << e.what() << endl;
+    return -1;
+  }
+}
---
+++ Basic2d-GLUT/glsupport.h
@@ -0,0 +1,272 @@
+#ifndef GLSUPPORT_H
+#define GLSUPPORT_H
+
+#include <iostream>
+#include <stdexcept>
+
+#include <GL/glew.h>
+#ifdef __MAC__
+# include <GLUT/glut.h>
+#else
+# include <GL/glut.h>
+#endif
+
+#define ASSERT(x) if (!(x)) { __debugbreak(); }
+#define GLCall(x) GLClearError();\
+				  x;\
+				  ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+
+void GLClearError();
+bool GLLogCall(const char* function, const char* file, int line);
+
+// Check if there has been an error inside OpenGL and if yes, print the error and
+// through a runtime_error exception.
+void checkGlErrors();
+
+// Reads and compiles a pair of vertex shader and fragment shader files into a
+// GL shader program. Throws runtime_error on error
+void readAndCompileShader(GLuint programHandle,
+                          const char *vertexShaderFileName, const char *fragmentShaderFileName);
+
+// Read and compile a pair of vertex shader and fragment shader source codes from
+// memory into a GL shader program. Throws runtime_error on error
+void readAndCompileShaderFromMemory(GLuint programHandle,
+                                    int vsSourceLength, const char *vsSource,
+                                    int fsSourceLength, const char *fsSource);
+
+// Link two compiled vertex shader and fragment shader into a GL shader program
+void linkShader(GLuint programHandle, GLuint vertexShaderHandle, GLuint fragmentShaderHandle);
+
+// Reads and compiles a single shader (vertex, fragment, etc) file into a GL
+// shader. Throws runtime_error on error
+void readAndCompileSingleShader(GLuint shaderHandle, const char* shaderFileName);
+
+// Reads and compiles a single shader (vertex, fragment, etc) from memory into a GL
+// shader. Throws runtime_error on error
+void readAndCompileSingleShaderFromMemory(GLuint shaderHandle,
+                                          int sourceLength, const char *source);
+
+
+// Classes inheriting Noncopyable will not have default compiler generated copy
+// constructor and assignment operator
+class Noncopyable {
+protected:
+  Noncopyable() {}
+  ~Noncopyable() {}
+private:
+  Noncopyable(const Noncopyable&);
+  const Noncopyable& operator= (const Noncopyable&);
+};
+
+// Light wrapper around a GL shader (can be geometry/vertex/fragment shader)
+// handle. Automatically allocates and deallocates. Can be casted to GLuint.
+class GlShader : Noncopyable {
+protected:
+  GLuint handle_;
+
+public:
+  GlShader(GLenum shaderType) {
+    handle_ = glCreateShader(shaderType); // create shader handle
+    if (handle_ == 0)
+      throw std::runtime_error("glCreateShader fails");
+    checkGlErrors();
+  }
+
+  ~GlShader() {
+    glDeleteShader(handle_);
+  }
+
+  // Casts to GLuint so can be used directly by glCompile etc
+  operator GLuint() const {
+    return handle_;
+  }
+};
+
+// Light wrapper around GLSL program handle that automatically allocates
+// and deallocates. Can be casted to a GLuint.
+class GlProgram : Noncopyable {
+protected:
+  GLuint handle_;
+
+public:
+  GlProgram() {
+    handle_ = glCreateProgram();
+    if (handle_ == 0)
+      throw std::runtime_error("glCreateProgram fails");
+    checkGlErrors();
+  }
+
+  ~GlProgram() {
+    glDeleteProgram(handle_);
+  }
+
+  // Casts to GLuint so can be used directly by glUseProgram and so on
+  operator GLuint() const {
+    return handle_;
+  }
+};
+
+
+// Light wrapper around a GL texture object handle that automatically allocates
+// and deallocates. Can be casted to a GLuint.
+class GlTexture : Noncopyable {
+protected:
+    GLuint handle_;
+
+public:
+    GlTexture() {
+        GLCall(glGenTextures(1, &handle_));
+        if (handle_ == 0) {
+            throw std::runtime_error("glGenTextures failed to generate texture.");
+        }
+        checkGlErrors();
+    }
+
+    // Accessor methods
+    GLuint getHandle() const {
+        return handle_;
+    }
+
+    ~GlTexture() {
+        glDeleteTextures(1, &handle_);
+    }
+
+    // Cast to GLuint for direct use with OpenGL functions
+    operator GLuint() const {
+        return handle_;
+    }
+};
+
+
+// Light wrapper around a GL buffer object handle that automatically allocates
+// and deallocates. Can be casted to a GLuint.
+class GlBufferObject : Noncopyable {
+protected:
+  GLuint handle_;
+
+public:
+  GlBufferObject() {
+    GLCall(glGenBuffers(1, &handle_));
+    checkGlErrors();
+  }
+
+  ~GlBufferObject() {
+    glDeleteBuffers(1, &handle_);
+  }
+
+  // Casts to GLuint so can be used directly glBindBuffer and so on
+  operator GLuint() const {
+    return handle_;
+  }
+};
+
+
+// Safe versions of various functions that handle GLSL shader attributes
+// and variables: These mainly issue a warning when specified attributes
+// and variables do not exist in the compiled GLSL program (e.g., due to
+// driver optimization), and return without doing anything when the user
+// tries to change these attributes or variables.
+// 
+// GLCall wraps the OpenGL function call and checks for errors by DS on August 6, 2024
+//
+inline GLint safe_glGetUniformLocation(const GLuint program, const char varname[]) {
+  GLint r = glGetUniformLocation(program, varname);
+  if (r < 0)
+    std::cerr << "WARN: "<< varname << " cannot be bound (it either doesn't exist or has been optimized away). safe_glUniform calls will silently ignore it.\n" << std::endl;
+  return r;
+}
+
+inline GLint safe_glGetAttribLocation(const GLuint program, const char varname[]) {
+  GLint r = glGetAttribLocation(program, varname);
+  if (r < 0)
+    std::cerr << "WARN: "<< varname << " cannot be bound (it either doesn't exist or has been optimized away). safe_glAttrib calls will silently ignore it.\n" << std::endl;
+  return r;
+}
+
+inline void safe_glUniformMatrix4fv(const GLint handle, const GLfloat data[]) {
+  if (handle >= 0)
+    GLCall(glUniformMatrix4fv(handle, 1, GL_FALSE, data));
+}
+
+inline void safe_glUniform1i(const GLint handle, const GLint a) {
+  if (handle >= 0) 
+    GLCall(glUniform1i(handle, a));   // GLCall is a macro that checks for errors
+}
+
+inline void safe_glUniform2i(const GLint handle, const GLint a, const GLint b) {
+  if (handle >= 0)
+    GLCall(glUniform2i(handle, a, b));
+}
+
+inline void safe_glUniform3i(const GLint handle, const GLint a, const GLint b, const GLint c) {
+  if (handle >= 0)
+    GLCall(glUniform3i(handle, a, b, c));
+}
+
+inline void safe_glUniform4i(const GLint handle, const GLint a, const GLint b, const GLint c, const GLint d) {
+  if (handle >= 0)
+    GLCall(glUniform4i(handle, a, b, c, d));
+}
+
+inline void safe_glUniform1f(const GLint handle, const GLfloat a) {
+  if (handle >= 0)
+    GLCall(glUniform1f(handle, a));
+}
+
+inline void safe_glUniform2f(const GLint handle, const GLfloat a, const GLfloat b) {
+  if (handle >= 0)
+    GLCall(glUniform2f(handle, a, b));
+}
+
+inline void safe_glUniform3f(const GLint handle, const GLfloat a, const GLfloat b, const GLfloat c) {
+  if (handle >= 0)
+    GLCall(glUniform3f(handle, a, b, c));
+}
+
+inline void safe_glUniform4f(const GLint handle, const GLfloat a, const GLfloat b, const GLfloat c, const GLfloat d) {
+  if (handle >= 0)
+    GLCall(glUniform4f(handle, a, b, c, d));
+}
+
+inline void safe_glEnableVertexAttribArray(const GLint handle) {
+  if (handle >= 0)
+    GLCall(glEnableVertexAttribArray(handle));
+}
+
+inline void safe_glDisableVertexAttribArray(const GLint handle) {
+  if (handle >= 0)
+    GLCall(glDisableVertexAttribArray(handle));
+}
+
+inline void safe_glVertexAttribPointer(const GLint handle, GLint size, GLenum type, GLboolean normalized, GLsizei stride, const GLvoid *pointer) {
+  if (handle >= 0)
+    GLCall(glVertexAttribPointer(handle, size, type, normalized, stride, pointer));
+}
+
+inline void safe_glVertexAttrib1f(const GLint handle, const GLfloat a) {
+  if (handle >= 0)
+    GLCall(glVertexAttrib1f(handle, a));
+}
+
+inline void safe_glVertexAttrib2f(const GLint handle, const GLfloat a, const GLfloat b) {
+  if (handle >= 0)
+    GLCall(glVertexAttrib2f(handle, a, b));
+}
+
+inline void safe_glVertexAttrib3f(const GLint handle, const GLfloat a, const GLfloat b, const GLfloat c) {
+  if (handle >= 0)
+    GLCall(glVertexAttrib3f(handle, a, b, c));
+}
+
+inline void safe_glVertexAttrib4f(const GLint handle, const GLfloat a, const GLfloat b, const GLfloat c, const GLfloat d) {
+  if (handle >= 0)
+    GLCall(glVertexAttrib4f(handle, a, b, c, d));
+}
+
+inline void safe_glVertexAttrib4Nub(const GLint handle, const GLubyte a, const GLubyte b, const GLubyte c, const GLubyte d) {
+  if (handle >= 0)
+    GLCall(glVertexAttrib4Nub(handle, a, b, c, d));
+}
+
+
+#endif
